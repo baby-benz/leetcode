fac27b549c0d6d14e81c93d999648b0aecd64df6
Nikita <baby0benz@gmail.com>
2022-07-24 21:28:20 +0300

    remove redundant param and memcpy calls

--- backtracking/46_permutations/permute.c
+++ backtracking/46_permutations/permute.c
@@ -20,22 +20,23 @@ void swap(int *nums, unsigned int x, unsigned int y) {
     nums[y] = temp;
 }
 
-void backtrackPermute(const int *nums, int **perms, int *curPerm, const unsigned int numsSize, const unsigned int start,
+void backtrackPermute(int **perms, int *curPerm, const unsigned int numsSize, const unsigned int start,
                       const unsigned int end, unsigned int *permsPos) {
     if (start == end) {
-        perms[*permsPos] = (int *) malloc(numsSize * sizeof(int));
-        memcpy(perms[*permsPos], curPerm, numsSize * sizeof(int));
+        perms[*permsPos] = (int *) malloc(sizeof(int) * numsSize);
+        memcpy(perms[*permsPos], curPerm,  sizeof(int) * numsSize);
         (*permsPos)++;
         return;
     }
     for (unsigned int i = start; i < end; i++) {
         swap(curPerm, start, i);
-        backtrackPermute(nums, perms, curPerm, numsSize, start + 1, end, permsPos);
+        backtrackPermute(perms, curPerm, numsSize, start + 1, end, permsPos);
         swap(curPerm, start, i);
     }
 }
 
-int **permute(const int *nums, const unsigned int numsSize, unsigned int *returnSize, unsigned int **returnColumnSizes) {
+int **
+permute(const int *nums, const unsigned int numsSize, unsigned int *returnSize, unsigned int **returnColumnSizes) {
     *returnSize = fact(numsSize);
     *returnColumnSizes = (unsigned int *) malloc(sizeof(unsigned int) * *returnSize);
 
@@ -43,18 +44,13 @@ int **permute(const int *nums, const unsigned int numsSize, unsigned int *return
         (*returnColumnSizes)[i] = numsSize;
     }
 
-    int **perms = malloc(sizeof(int *) * *returnSize);
-
-    for (int i = 0; i < *returnSize; i++) {
-        perms[i] = (int *) malloc(sizeof(int) * numsSize);
-    }
-
+    int **perms = (int **) malloc(sizeof(int *) * *returnSize);
     int *curPerm = (int *) malloc(sizeof(int) * numsSize);
-    memcpy(curPerm, nums,numsSize * sizeof(int));
+    memcpy(curPerm, nums, sizeof(int) * numsSize);
 
     unsigned int permsPos = 0;
 
-    backtrackPermute(nums, perms, curPerm, numsSize, 0, numsSize, &permsPos);
+    backtrackPermute(perms, curPerm, numsSize, 0, numsSize, &permsPos);
 
     return perms;
 }
